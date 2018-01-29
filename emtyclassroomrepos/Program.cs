a6024149628fecf6a4f07e6688898f773de6c805
HassanRH <hassan---06@hotmail.com>
2018-01-29 10:56:00 +0100

    Add project files.

---
+++ emtyclassroomrepos/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace emtyclassroomrepos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
